49d8ea16a6228c3cb59d86b442476f41c40e7c73
Gustavo Olivio <gustavounk@gmail.com>
2020-07-16 17:12:05 -0300

    Creating Projects

---
+++ FiTCARD.Establishments/FiTCARD.Establishments.Web/Controllers/Teste.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiTCARD.Establishments.Web.Controllers
+{
+    /// <summary>
+    /// Test Controller
+    /// </summary>
+    [Route("api/[controller]")]
+    public class Teste : Controller
+    {
+        /// <summary>
+        /// Test
+        /// </summary>
+        /// <returns></returns>
+        [Route("Index"), HttpPost]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
